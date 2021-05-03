e70c88301a1f3bf324ec790522d91c158c783347
kwadwo485 <57927146+kwadwo485@users.noreply.github.com>
2021-05-03 03:52:14 -0400

    Initial commit

---
+++ src/main/java/com/kgd/secretNotes2/SecretNotes2Application.java
@@ -0,0 +1,13 @@
+package com.kgd.secretNotes2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretNotes2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretNotes2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kgd/secretNotes2/SecretNotes2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgd.secretNotes2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretNotes2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
